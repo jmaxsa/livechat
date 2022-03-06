89b834ba2a8d7a0d1a8b184a40a51f2b1effb740
Jonathan Andrade <85408369+jmaxsa@users.noreply.github.com>
2022-03-06 16:08:36 -0300

    Adding colors

--- app/src/main/java/br/com/jm/livechat/base/BaseActivity.kt
+++ app/src/main/java/br/com/jm/livechat/base/BaseActivity.kt
@@ -6,12 +6,4 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import br.com.jm.livechat.R
 
-abstract class BaseActivity: AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val rootView by lazy { findViewById<View>(android.R.id.content) }
-        rootView.setBackgroundColor(ResourcesCompat.getColor(resources, R.color.blue_500, null))
-    }
-}
+abstract class BaseActivity: AppCompatActivity() {}
