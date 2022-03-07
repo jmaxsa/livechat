4286eb45b41e0ef70f0b5496717fafc80e473a54
Jonathan Andrade <85408369+jmaxsa@users.noreply.github.com>
2022-03-07 20:14:37 -0300

    Adding google firebase auth

---
+++ app/src/main/java/br/com/jm/livechat/SignUpActivity.kt
@@ -0,0 +1,48 @@
+package br.com.jm.livechat
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity: AppCompatActivity(R.layout.activity_sign_up) {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createSignInIntent()
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            //TODO Save user in local protected session
+            val user = FirebaseAuth.getInstance().currentUser
+            startActivity(Intent(this, HomeActivity::class.java))
+        } else {
+            //TODO Handle error code in response
+//            when (response?.error?.errorCode)
+        }
+    }
+}
--- app/src/main/java/br/com/jm/livechat/components/CustomToolbar.kt
+++ app/src/main/java/br/com/jm/livechat/components/CustomToolbar.kt
@@ -2,14 +2,35 @@ package br.com.jm.livechat.components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import br.com.jm.livechat.R
 
 class CustomToolbar @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
+    private val attrs: AttributeSet? = null,
     defStyle: Int = 0,
 ): Toolbar(context, attrs, defStyle) {
+
+    private val searchButton by lazy { findViewById<ImageButton>(R.id.search_button) }
+
+    override fun onViewAdded(child: View?) {
+        super.onViewAdded(child)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomToolbar,
+            0, 0
+        ).apply {
+            try {
+                searchButton.isVisible = getBoolean(R.styleable.CustomToolbar_showSearchIcon, true)
+            } finally {
+                recycle()
+            }
+        }
+    }
 
     init {
         inflate(context, R.layout.layout_custom_toolbar, this)
